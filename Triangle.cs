3ead1741d46dbca0f1570dcec05a9d56ce0fc95c
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-14 11:22:16 +0300

    Is  it a triangle

---
+++ Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItATriangle
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            a = sideA;
+            b = sideB;
+            c = sideC;
+        }
+
+        public string ReallyTriangle()
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return "It is a triangle";
+            }
+            else
+            {
+                return "It is not a triangle";
+            }
+        }
+    }
+}
